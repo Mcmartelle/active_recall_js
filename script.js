bc2f8a32cf398451f8920864323769d5f6f34d30
Mcmartelle <mcmartelle@gmail.com>
2021-05-19 17:10:46 -0700

    voice is working

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var root = document.body;
+var synth = window.speechSynthesis;
 
-var QA_PAIRS = [
+
+
+const QA_PAIRS = [
   {
     q: "What is 2 + 2?",
     a: "4"
@@ -22,6 +25,51 @@ var question = "";
 var answer = "";
 var answerAttempt = "";
 var feedback = "";
+var voice = {};
+var voices = [];
+var rate = 1;
+var pitch = 1;
+
+
+function populateVoiceList() {
+  voices = synth.getVoices().sort(function (a, b) {
+    const aname = a.name.toUpperCase(), bname = b.name.toUpperCase();
+    if ( aname < bname ) return -1;
+    else if ( aname == bname ) return 0;
+    else return +1;
+  });
+  console.log('populateVoiceList');
+  // console.log('voices: ', voices);
+  if (voices.length > 0) {
+    m.mount(document.getElementById('speech_synth_options'), speechSynthesisOptions);
+    setNextQuestion();
+  }
+  // var selectedIndex = voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
+  // voiceSelect.innerHTML = '';
+  // for(i = 0; i < voices.length ; i++) {
+    // var option = document.createElement('option');
+    // option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+    // 
+  //   if(voices[i].default) {
+  //     option.textContent += ' -- DEFAULT';
+  //   }
+
+  //   option.setAttribute('data-lang', voices[i].lang);
+  //   option.setAttribute('data-name', voices[i].name);
+  //   voiceSelect.appendChild(option);
+  // }
+  // voiceSelect.selectedIndex = selectedIndex;
+}
+
+populateVoiceList()
+
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+}
+
+function doNothing() {
+  // nothing being done
+}
 
 function setNextQuestion() {
   feedback = "";
@@ -31,8 +79,10 @@ function setNextQuestion() {
     qaPair =  qaPairs[qaPairIndex];
     question = qaPair.q;
     answer = qaPair.a;
+    speak(question, doNothing);
   } else {
-    feedback = "Congratulations! You've Actively Recalled every single concept!"
+    feedback = "Congratulations! You've Actively Recalled every concept!"
+    speak(feedback);
   }
   m.redraw();
 }
@@ -44,14 +94,11 @@ function populateQaPairs(orig) {
 function submitAnswer() {
   if (answerAttempt === answer) {
     qaPairs.splice(qaPairIndex, 1);
-    feedback = `"${answerAttempt}" is the correct answer`;
+    feedback = `"${answerAttempt}" is the correct answer!`;
   } else {
     feedback = `"${answerAttempt}" is incorrect. The correct answer is "${answer}"`;
   }
-  window.setTimeout(function() {
-    console.log('wtf');
-    setNextQuestion();
-  }, 3000);
+  speak(feedback, setNextQuestion);
 }
 
 var answerForm = {
@@ -77,15 +124,47 @@ var speechSynthesisOptions = {
     return m("form", {class: "voice_options"}, [
       m("h2", "Voice Options"),
       m("label.label[for=rate]", "Rate"),
-      m("input.input[type=range][min=0][max=2][value=1][step=0.1][id=rate]", {class: "speech_range"}),
+      m("span", {class: "rate-value"}, ""),
+      m("input.input[type=range][min=0][max=2][step=0.1][id=rate]",
+        {
+          id: "rate",
+          class: "speech_range",
+          value: rate,
+          onchange: e => {
+            console.log(e.target.value);
+            rate = e.target.value;
+            speak("This is a how my voice sounds now.")
+          }
+      }),
       m("label.label[for=pitch]", "Pitch"),
-      m("input.input[type=range][min=0][max=2][value=1][step=0.1][id=pitch]", {class: "speech_range"})
+      m("span", {class: "pitch-value"}, ""),
+      m("input.input[type=range][min=0][max=2][step=0.1][id=pitch]",
+        {
+          id: "pitch",
+          class: "speech_range",
+          value: pitch,
+          onchange: e => {
+            console.log(e.target.value);
+            pitch = e.target.value;
+            speak("This is a how my voice sounds now.")
+          }
+        }
+      ),
+      m('label[for=voice]', 'Voice'),
+      m('select[name=voice]', {
+        value: voice,
+        onchange: e => {
+          voice = voices[e.target.selectedIndex];
+          speak("This is a how my voice sounds now.")
+        }
+      }, [
+        voices.map(i => m('option', { value: i, "data-lang": i.lang, "data-name": i.name }, i.name + ' (' + i.lang + ')' + (i.default ? " -- DEFAULT": '')))
+      ])
     ]);
   }
 }
 
 var Main = {
-  oninit: setNextQuestion(),
   view: function() {
     return m("main", [
       m("h1", "Active Recall"),
@@ -96,9 +175,7 @@ var Main = {
           m(answerForm),
           m("p", {class: "feedback"}, feedback)
         ]),
-        m("section", [
-          m(speechSynthesisOptions)
-        ])
+        m("section", {id: "speech_synth_options"})
       ])
     ])
   }
@@ -112,10 +189,47 @@ var Splash = {
   }
 }
 
-m.route(root, "/splash", {
+m.route(root, "/main", {
   "/splash": Splash,
   "/main": Main,
 })
 
 
+  
+var voiceSelect = document.querySelector('select');
 
+var pitch = document.querySelector('#pitch');
+var pitchValue = document.querySelector('.pitch-value');
+var rate = document.querySelector('#rate');
+var rateValue = document.querySelector('.rate-value');
+
+
+
+function speak(text, onend){
+  if (synth.speaking) {
+    console.error('speechSynthesis.speaking');
+    return;
+  }
+  if (text !== '' && voice && voice.name) {
+    var utterThis = new SpeechSynthesisUtterance(text);
+    utterThis.onend = function (event) {
+      console.log('SpeechSynthesisUtterance.onend');
+      if (typeof onend === 'function') {
+        onend(event);
+      }
+    }
+    utterThis.onerror = function (event) {
+        console.error('SpeechSynthesisUtterance.onerror');
+    }
+    console.log(voice);
+    for(i = 0; i < voices.length ; i++) {
+      if(voices[i].name === voice.name) {
+        utterThis.voice = voices[i];
+        break;
+      }
+    }
+    utterThis.pitch = pitch;
+    utterThis.rate = rate;
+    synth.speak(utterThis);
+  }
+}
